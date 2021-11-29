e22d574fd778102b72ddd81c9ca418edb2a20d5f
Livis <alexei055.kozachenko@gmail.com>
2021-11-29 22:10:41 +0300

    исправление

--- variant_1.py
+++ variant_1.py
@@ -2,31 +2,33 @@ import time
 
 
 def my_decorator(func):
-    def decor():
+    def decor(*args, **kwargs):
         start = time.perf_counter()
-        func()
+        func(*args, **kwargs)
         print(f"Была вызвана функция {func.__name__}\n"
               f"Время затраченное на выполение: {time.perf_counter()-start}")
     return decor
 
 
 @my_decorator
-def calc_sqft_per_acre():
-    length = int(input("Введите длину участка в футах: "))
-    width = int(input("Введите ширину участка в футах: "))
+def calc_sqft_per_acre(length, width):
     print(f"Площадь в акрах: {(length*width)/43560}")
 
 
 @my_decorator
-def calc_free_fall():
+def calc_free_fall(d):
     vi = 0
     a = 9.8
-    d = int(input("Введите высоту в метрах с которой упадет объект: "))
     vf = abs(vi) + 2 * a * d
     print(f"Финальная скорость: {vf} м/с")
 
 
-calc_sqft_per_acre()
-calc_free_fall()
+length = int(input("Введите длину участка в футах: "))
+width = int(input("Введите ширину участка в футах: "))
+
+calc_sqft_per_acre(length, width)
+
+d = int(input("Введите высоту в метрах с которой упадет объект: "))
+calc_free_fall(d)
 
 
